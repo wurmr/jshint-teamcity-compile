6bd0ecbe5de460883d9299db97f3a38edf067edb
Jim Karg <jkarg@stonegatemtg.com>
2014-10-07 14:44:22 -0500

    Added npm tests and updated code style

--- teamcity.js
+++ teamcity.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
 function escapeTeamcityString(message) {
   if (!message) {
-    return "";
+    return '';
   }
 
-  return message.replace(/\|/g, "||")
-    .replace(/\'/g, "|\'")
-    .replace(/\n/g, "|n")
-    .replace(/\r/g, "|r")
-    .replace(/\u0085/g, "|x")
-    .replace(/\u2028/g, "|l")
-    .replace(/\u2029/g, "|p")
-    .replace(/\[/g, "|[")
-    .replace(/\]/g, "|]");
+  return message.replace(/\|/g, '||')
+    .replace(/\'/g, '|\'')
+    .replace(/\n/g, '|n')
+    .replace(/\r/g, '|r')
+    .replace(/\u0085/g, '|x')
+    .replace(/\u2028/g, '|l')
+    .replace(/\u2029/g, '|p')
+    .replace(/\[/g, '|[')
+    .replace(/\]/g, '|]');
 }
 
 module.exports = {
@@ -29,25 +29,26 @@ module.exports = {
       }
 
       previous[current.file].push({
-        name: escapeTeamcityString(current.file + ": line " + error.line + ", col " + error.character + ", " + error.reason),
-        message: escapeTeamcityString(error.code + ": " + error.reason),
+        name: escapeTeamcityString(current.file + ': line ' + error.line +
+					', col ' + error.character + ', ' + error.reason),
+        message: escapeTeamcityString(error.code + ': ' + error.reason),
         detailed: escapeTeamcityString(error.evidence)
       });
 
       return previous;
     }, {});
 
-		output.push("##teamcity[compilationStarted compiler='jshint']");
+    output.push('##teamcity[compilationStarted compiler=\'jshint\']');
     Object.keys(errors).forEach(function(key) {
       errors[key].forEach(function(test) {
-				var messageDetails = test.message + ": " + test.detailed;
-				output.push("##teamcity[message text='" + test.name + "' errorDetails='" + messageDetails + "' status='ERROR']");
+        var messageDetails = test.message + ': ' + test.detailed;
+        output.push('##teamcity[message text=\'' + test.name +
+					'\' errorDetails=\'' + messageDetails + '\' status=\'ERROR\']');
       });
     });
-
-		output.push("##teamcity[compilationFinished compiler='jshint']");
+    output.push('##teamcity[compilationFinished compiler=\'jshint\']');
 
     // Print to process.stdout
-    console.log(output.join("\n"));
+    console.log(output.join('\n'));
   }
 };
